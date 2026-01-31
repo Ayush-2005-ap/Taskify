4dff5938a8a576e81eb6b91e05a481fddf99453f
Ayush Pandey <apayush.20@gmail.com>
2026-02-01 00:25:37 +0530

    Update API baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5050",
+  baseURL: "https://taskify-backend-twn6.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
